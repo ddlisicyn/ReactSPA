4e17ec306eb1ab2ae5a770b098848c17d8a8200a
Daniil Lisitsyn <ddlisicyn@mail.ru>
2021-07-31 01:06:31 +0500

    photos rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState} from 'react';
+
+class Photo extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <img src={this.props?.url} alt="image 150x150"/>
+        <p>{this.props.albumId}</p>
+        <a href="">link</a>
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => response.json())
+      /* .then(json => setState({url: json[0]["thumbnailUrl"]})) */
+      .then(json => setPhotos(json));
+  }, []);
+  console.log(photos);
+  return photos.map((photo) => (
+    <Photo url={photo.thumbnailUrl} albumId={photo.albumId} />
+  ));
 }
 
 export default App;
